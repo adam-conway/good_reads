9d3b09f676c6273a6060680fef5ef8c22d4f5ee4
adam-conway <adam.n.conway@gmail.com>
2018-03-30 10:59:59 -0600

    Restricted routes to what I need

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :books, only: [:show]
 
-  resources :users do
-    resources :reviews
+  resources :users, only: [:create, :new] do
+    resources :reviews, only: [:create, :new]
   end
 
 end
