90bd8be998f564789d0742e84b5ae5d4d1c90230
adam-conway <adam.n.conway@gmail.com>
2018-03-30 10:26:19 -0600

    Finally got all config stuff working

---
+++ spec/books/user_sees_a_book_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "User sees one book" do
+  scenario "a user sees a book" do
+    book = Book.create!(title: "This is a book title", )
+    user = User.create!(name: "Adam")
+    review = user.reviews.create!(body: "This is a crappy book", rating: 5, book_id: book.id)
+
+    visit book_path(book)
+
+    expect(current_path).to eq(book_path(book))
+    expect(page).to have_content(book.title)
+    expect(page).to have_content (review.body)
+    expect(page).to have_content (review.author)
+    expect(page).to have_content (review.rating)
+  end
+end
